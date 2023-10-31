6212ca662189a6899b2d8adb1eaded6b6db54fb5
ikramiste <ikrame.saadi@gmail.com>
2023-10-31 14:28:45 +0100

    created index for routing

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,4 +2,8 @@ class GossipsController < ApplicationController
     def show
     @gossip = Gossip.includes(:user).find(params[:id])
     end
+
+    def index
+      @gossips = Gossip.all
+    end
   end
