7038cd05e58d7d222e9ccded991c159ac18dd8bb
ikramiste <ikrame.saadi@gmail.com>
2023-11-02 13:48:07 +0100

    added creation of user possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+          log_in(@user)
+          redirect_to root_url
+        else
+          render 'new'
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :city_id)
+    end
 end
